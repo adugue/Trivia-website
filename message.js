f3f7eb01dc070c3c8c627596b181963468e5915e
A. Dugue <76267790+adug-git@users.noreply.github.com>
2021-06-07 16:13:56 -0400

    Make the buttons reset to their default color between answers

--- message.js
+++ message.js
@@ -1,27 +1,44 @@
 // USING jQuery:
 
 $(document).ready(function() {   // allows the following code to run as soon as the page is loaded
+    const defaultButtonColor = $("button").css("background-color");
+
+    function wrongAnswer(answer) {
+        $(answer).css("background", "red");
+        $("#answer").html("Incorrect");
+    }
+
+    function rightAnswer(answer) {
+        $(answer).css("background", "LightGreen");
+        $("#answer").html("Correct!");
+    }
+
+    function resetColors() {
+        $("#wrong1").css("background", defaultButtonColor);
+        $("#wrong2").css("background", defaultButtonColor);
+        $("#wrong3").css("background", defaultButtonColor);
+        $("#right").css("background", defaultButtonColor);
+    }
 
     $("#wrong1").click(function(){
-        $("#wrong1").css("background", "red");
-        $("#answer").html("Incorrect");
+        resetColors();
+        wrongAnswer("#wrong1");
     });
 
     $("#wrong2").click(function(){
-        $("#wrong2").css("background", "red");
-        $("#answer").html("Incorrect");
+        resetColors();
+        wrongAnswer("#wrong2");
     });
 
     $("#wrong3").click(function(){
-        $("#wrong3").css("background", "red");
-        $("#answer").html("Incorrect");
+        resetColors();
+        wrongAnswer("#wrong3");
     });
 
     $("#right").click(function(){
-        $("#right").css("background", "LightGreen");
-        $("#answer").html("Correct!");
+        resetColors();
+        rightAnswer("#right");
     });
-
 
     $("form").submit(function(){
         let ans = $("#textanswer").val(); // get the value from the text field
